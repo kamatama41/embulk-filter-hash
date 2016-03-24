c4acb356219a551d98a7a8945f34ea60c8ab704d
Shinichi Ishimura <shiketaudonko41@gmail.com>
2016-03-24 18:47:58 +0900

    Stop wildcard import

--- src/main/java/org/embulk/filter/hash/HashFilterPlugin.java
+++ src/main/java/org/embulk/filter/hash/HashFilterPlugin.java
@@ -2,8 +2,20 @@ package org.embulk.filter.hash;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Throwables;
-import org.embulk.config.*;
-import org.embulk.spi.*;
+import org.embulk.config.Config;
+import org.embulk.config.ConfigDefault;
+import org.embulk.config.ConfigSource;
+import org.embulk.config.Task;
+import org.embulk.config.TaskSource;
+import org.embulk.spi.Column;
+import org.embulk.spi.DataException;
+import org.embulk.spi.Exec;
+import org.embulk.spi.FilterPlugin;
+import org.embulk.spi.Page;
+import org.embulk.spi.PageBuilder;
+import org.embulk.spi.PageOutput;
+import org.embulk.spi.PageReader;
+import org.embulk.spi.Schema;
 import org.embulk.spi.time.Timestamp;
 import org.embulk.spi.type.Types;
 
